c845778991e27e5db0750d570f15da52b25821a6
Bob Weichler <bobmasaki@hotmail.com>
2016-07-23 10:24:00 +0200

    team member design

--- templates/home.php
+++ templates/home.php
@@ -12,7 +12,73 @@
 				<?php include_once('list.php'); ?>
 			</div><!-- ./list -->
 			<div class="members">
+				<div class="search">
+					<div class="input-group">
+						<input type="text" class="form-control" placeholder="Zoek teamgenoot">
+						<span class="input-group-btn">
+							<button class="btn btn-default" type="button"><i class="fa fa-search" aria-hidden="true"></i></button>
+						</span>
+					</div><!-- /input-group -->
+				</div><!-- ./search -->
 
+				<ul>
+					<li>
+						<img src="https://randomuser.me/api/portraits/women/80.jpg" alt="Member" >
+						<p>
+							<strong>Bob Weichler</strong>
+							<span>Front-end developer</span>
+						</p>
+						<a href="#!"><i class="fa fa-external-link" aria-hidden="true"></i></a>
+					</li>
+					<li>
+						<img src="https://randomuser.me/api/portraits/women/80.jpg" alt="Member" >
+						<p>
+							<strong>Bob Weichler</strong>
+							<span>Front-end developer</span>
+						</p>
+						<a href="#!"><i class="fa fa-external-link" aria-hidden="true"></i></a>
+					</li>
+					<li>
+						<img src="https://randomuser.me/api/portraits/women/80.jpg" alt="Member" >
+						<p>
+							<strong>Bob Weichler</strong>
+							<span>Front-end developer</span>
+						</p>
+						<a href="#!"><i class="fa fa-external-link" aria-hidden="true"></i></a>
+					</li>
+					<li>
+						<img src="https://randomuser.me/api/portraits/women/80.jpg" alt="Member" >
+						<p>
+							<strong>Bob Weichler</strong>
+							<span>Front-end developer</span>
+						</p>
+						<a href="#!"><i class="fa fa-external-link" aria-hidden="true"></i></a>
+					</li>
+					<li>
+						<img src="https://randomuser.me/api/portraits/women/80.jpg" alt="Member" >
+						<p>
+							<strong>Bob Weichler</strong>
+							<span>Front-end developer</span>
+						</p>
+						<a href="#!"><i class="fa fa-external-link" aria-hidden="true"></i></a>
+					</li>
+					<li>
+						<img src="https://randomuser.me/api/portraits/women/80.jpg" alt="Member" >
+						<p>
+							<strong>Bob Weichler</strong>
+							<span>Front-end developer</span>
+						</p>
+						<a href="#!"><i class="fa fa-external-link" aria-hidden="true"></i></a>
+					</li>
+					<li>
+						<img src="https://randomuser.me/api/portraits/women/80.jpg" alt="Member" >
+						<p>
+							<strong>Bob Weichler</strong>
+							<span>Front-end developer</span>
+						</p>
+						<a href="#!"><i class="fa fa-external-link" aria-hidden="true"></i></a>
+					</li>
+				</ul>
 			</div><!-- ./members -->
 		</div><!-- ./app -->
 	</div><!-- ./content -->
